c89d92cfde169f7448e21a019a926b9014ceee41
matin <m.nazari@fakour.net>
2023-06-25 10:08:24 +0330

    api description added

--- src/main/java/com/matin/adminpanel/configuration/AdminControlSwitchConfiguration.java
+++ src/main/java/com/matin/adminpanel/configuration/AdminControlSwitchConfiguration.java
@@ -1,16 +1,19 @@
 package com.matin.adminpanel.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "api.capabilities")
+@Slf4j
 public class AdminControlSwitchConfiguration {
 
     private boolean adminControlSwitch;
 
     public void setAdminControlSwitch(boolean adminControlSwitch) {
         this.adminControlSwitch = adminControlSwitch;
+        log.info("Admin Control Switch status has been changed to " + adminControlSwitch);
     }
 
     public boolean isAdminControlSwitch() {
--- src/main/java/com/matin/adminpanel/controller/AdminControlSwitchController.java
+++ src/main/java/com/matin/adminpanel/controller/AdminControlSwitchController.java
@@ -1,6 +1,7 @@
 package com.matin.adminpanel.controller;
 
 import com.matin.adminpanel.configuration.AdminControlSwitchConfiguration;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +16,18 @@ public class AdminControlSwitchController {
     }
 
     @PostMapping("/admin-switch")
+    @Operation(summary = "Updates admin switch control"
+            , description = "This API updates the admin switch control in RUNTIME. " +
+            "pls notice that the value by default is set to true and when changed through this api" +
+            "the change will not be persisted.")
     public ResponseEntity<String> updateAdminSwitch(@RequestParam boolean newSwitchValue) {
         adminControlSwitchConfiguration.setAdminControlSwitch(newSwitchValue);
         return ResponseEntity.ok("Admin switch updated successfully.");
     }
 
     @GetMapping("/adminControlSwitch")
+    @Operation(summary = "Returns the admin control switch value"
+            , description = "Returns the admin control switch value")
     public ResponseEntity<Boolean> getAdminControlSwitch() {
         boolean adminControlSwitch = adminControlSwitchConfiguration.isAdminControlSwitch();
         return ResponseEntity.ok(adminControlSwitch);
